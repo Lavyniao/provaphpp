30bbd4575107285acf86e90afc0a54976f95434d
Lavyniao <gustavoinfomax@yahoo.com.br>
2024-12-05 08:39:07 -0300

    terminer a prova

---
+++ index.php
@@ -0,0 +1,32 @@
+     <h2>receita:
+         <?php
+           echo "brigadeiro"
+        ?>
+        </h2>
+        <table border="1">
+           <thead>
+             <tr>
+                 <th> ingredientes</th>
+                 <th>quandidade</th>
+              </tr>
+    </thead>
+                  <tboody>
+                  <tr>
+                      <td>leite condensado</td>
+                      <td>1 caixa</td>
+                  </tr>
+                  <tr>
+                      <td>colheres de mantega</td>
+                      <td>2</td>
+                  </tr>
+                <tr>
+                    <td> chocolate</td>
+                    <td>2</td>
+    </tr>
+    </tbody>
+    </table>
+    <h1>nota:</h1>
+    <script>
+        var h1=document.querySelector("h1");
+        h1.innerHTML="nota:9";
+    </Script>
